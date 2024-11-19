e8f90983e131d86b6a69da2589dcb098be7299f6
retr0lbb <retr0lbb@gmail.com>
2024-11-19 16:01:37 -0300

    fix: add no cache to storage

--- app/api/get-valid-prompts/route.ts
+++ app/api/get-valid-prompts/route.ts
@@ -4,8 +4,11 @@ import { prisma } from "@/app/api/utils/prima-client"
 export async function GET() {
     try {
         const messagePropts = await prisma.message.findMany()
+        console.log("Fetched data: ", messagePropts);
+        const response = NextResponse.json(messagePropts)
+        response.headers.set("Cache-Control", "no-store")
 
-        return NextResponse.json(messagePropts)
+        return response
     } catch (error) {
 
         return NextResponse.json(
