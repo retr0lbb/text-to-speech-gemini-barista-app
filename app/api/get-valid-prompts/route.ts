a436c4418096977f970a6692f04a388edcba0dd4
retr0lbb <retr0lbb@gmail.com>
2024-11-20 20:25:40 -0300

    build: 1.1.0

--- app/api/get-valid-prompts/route.ts
+++ app/api/get-valid-prompts/route.ts
@@ -4,15 +4,12 @@ import { prisma } from "@/app/utils/prima-client"
 export async function GET() {
     try {
         const messagePropts = await prisma.message.findMany()
-        console.log("Fetched data: ", messagePropts);
         const response = NextResponse.json(messagePropts)
         response.headers.set("Cache-Control", "no-store")
-
         return response
+        
     } catch (error) {
 
-        return NextResponse.json(
-            { status: 400, error} 
-        );
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
     }
 }
